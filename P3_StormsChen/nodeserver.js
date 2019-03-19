3d268aea03442cf058eaf52f2b880f3240f51023
Jacob Storms <jacob.storms@uky.edu>
2019-03-19 13:43:57 -0400

    copied some base code from example

--- P3_StormsChen/nodeserver.js
+++ P3_StormsChen/nodeserver.js
@@ -1,1 +1,22 @@
-//test
+const whereToListen = 'belgarath.cs.uky.edu';
+const port = 3332;
+
+function doprocess(request, response){
+  var xurl = request.url;
+  response.statusCode = 200;
+  response.setHeader('Content-Type', 'text/plain');
+  response.end('Hello, World! You requested the following URL: '+xurl+'\n');
+  console.log("Hey, the client requested the URL: ("+xurl+")");
+}
+
+var server = http.createServer(doprocess);
+
+try{
+  server.on('error', (e) => {
+    console.log("Error! " +e.code);
+  }); // server.on()
+  server.listen(port,whereToListen);
+  console.log("Listening.....");
+} catch(error){
+  console.log("There was an error!" +error);
+}
