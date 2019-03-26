5c363bfeff67f1bfd2740c48f6c6cd10bb663d8d
Jacob Storms <jacob.storms@uky.edu>
2019-03-26 13:35:07 -0400

    started case handling

--- P3_StormsChen/nodeserver.js
+++ P3_StormsChen/nodeserver.js
@@ -1,20 +1,9 @@
 var XelkReq = require('./P3_Req.js');
-var http = require('http'),
-    url = require('url');
-
-function getURL(request, response) {
-    var xurl = request.url;
-    response.statusCode = 200;
-    response.setHeader('Content-Type', 'text/plain');
-    response.end('Hello, World! You requested the following URL: '+xurl+'\n');
-    console.log("Hey, the client requested the URL: ("+xurl+")");
-    if (urlaccepted){
-
-    }
-}
+var http = require('http');
+var url = require('url');
 
 function process_url(input) {
-
+    console.log("processingurl:", input);
     // regex split up into parts
     var localFileString = "^(\/LOCALFILE\/)";
     var localExecString = "^(\/LOCALEXEC\/)";
@@ -27,41 +16,51 @@ function process_url(input) {
     var extAllowed = "(";
     XelkReq.extAllowed().forEach(function(item){
       var temp = "(\\".concat(item[0]).concat(")|");
-      //console.log(temp);
       extAllowed = extAllowed.concat(temp);
 
     });
-    extAllowed = (extAllowed.substring(0, extAllowed.length - 1)).concat(")")
-
-    console.log(extAllowed);
-    //ask chen about the differences in what this creates vs the one below
-    var extAllowed = "((\.html)|(\.mp3)|(\.jpg))";
-    console.log(extAllowed);
+    extAllowed = (extAllowed.substring(0, extAllowed.length - 1)).concat(")$")
 
     // example of creating new regex and testing it with a string
     var regex1 = new RegExp(localFileString + filepathString + extAllowed);
-    var string1a = "/LOCALFILE/myfile.html";
-    var string1b = "/LOCALFILE/myfile/test.html";
-    var string1c = "";
-    console.log(regex1.test(string1a)); // this one is true
-    console.log(regex1.test(string1b)); // this one is true
-    console.log(regex1.test(string1c)); // this one is false
 
+    var regex2 = new RegExp(remoteFileString + filepathString + extAllowed);
+    /*
+    var string2a = "/REMOTEFILE/myfile.html";
+    console.log(regex2.test(string2a)); //
+    */
+    var regex3 = new RegExp(localExecString + filepathString + extAllowed);
+    
+    var regex4 = new RegExp(remoteExecString + filepathString + extAllowed);
     //now we should know if the command was valid and if so what type it is
-
-
+    var type = 0;
+    if (regex1.test(input) == true){
+      type = 1;
+    }
+    else if(regex2.test(input) == true){
+      type = 2;
+    }
+    else if(regex3.test(input) == true){
+      type = 3;
+    }
+    else if(regex4.test(input) == true){
+      type = 4;
+    }
+    //if it doesn't fall into any of these cases, i.e., it doesnt pass anything then it remains 0
     // 1. return validity and type
+    return type;
 }
 function getURL(request, response) {
+    console.log("request:", request.url);
+
     var xurl = request.url;
     response.statusCode = 200;
     response.setHeader('Content-Type', 'text/plain');
     response.end('Hello, World! You requested the following URL: '+xurl+'\n');
     console.log("Hey, the client requested the URL: ("+xurl+")");
     //2. var = process_url(xurl)
-    if (urlaccepted){
-      //3. do something
-    }
+
+    console.log(process_url(xurl));
 }
 function serveFile() {
 
@@ -98,5 +97,5 @@ try{
 } catch(error) {
     console.log("There was an error!" +error);
 }
-process_url("test")
+
 */
