c9f0a737c99652fe2e2d09f14a9685a85b0e79d9
Jacob Chen <jiayichen@Chens-MacBook-Pro.local>
2019-03-27 13:52:08 -0400

    cleaned up code, created functions to reduce duplicate code, added comments

--- P3_StormsChen/nodeserver.js
+++ P3_StormsChen/nodeserver.js
@@ -1,236 +1,234 @@
-var XelkReq = require('./P3_Req.js');
+/* CS 316
+   Project 3
+   by William Storms and Jia-Yi Chen
+   March 27, 2019
+*/
+
+// Including modules and methods required for project
+var XelkReq = require('/homes/paul/HTML/CS316/P3_Req.js');
 var http = require('http');
 var url = require('url');
-var fs = require('fs');
-var child_process = require('child_process');
-//testing git merge stuff
-function process_url(input) {
+var fs = require('fs').readFile;
+var exec = require('child_process').exec;
 
-    // regex split up into parts
-    var localFileString = "^(\/LOCALFILE\/)";
-    var localExecString = "^(\/LOCALEXEC\/)";
-    var remoteFileString = "^(\/REMOTEFILE\/)";
-    var remoteExecString = "^(\/REMOTEEXEC\/)";
-    var filepathString = "[a-zA-Z0-9\/\~]+";
+
+// Defines the hostname
+const hostname = 'iris.cs.uky.edu';
+const port = generatePort(); // Use Randomport
+
+
+function processUrl(inputURL) {
+/* Function that takes in URL input and ensures it is a valid HTTP requests by
+   checking the format with regular expressions using .test() method. */
+
+    // RegEx strings split up into parts
+    // XelkReq.extAllowed() used to form a separate extensions variable string
+    var extAllowed = "(";
+    XelkReq.extAllowed().forEach(function(item) {
+        var temp = "(\\".concat(item[0]).concat(")|");
+        extAllowed = extAllowed.concat(temp);
+    });
+    extAllowed = (extAllowed.substring(0, extAllowed.length - 1)).concat(")$");
+    var filepathString = "[a-zA-Z0-9\/\~]+" + extAllowed;
     var hostString = "([a-zA-Z0-9\.]+)";
     var cgipathString = "[a-zA-Z0-9\/\~]+(\.cgi)$";
 
-    var extAllowed = "(";
-    XelkReq.extAllowed().forEach(function(item){
-      var temp = "(\\".concat(item[0]).concat(")|");
-      extAllowed = extAllowed.concat(temp);
+    var localFileString = "^(\/LOCALFILE\/)" + filepathString;
+    var localExecString = "^(\/LOCALEXEC\/)" + cgipathString;
+    var remoteFileString = "^(\/REMOTEFILE\/)" + hostString + filepathString;
+    var remoteExecString = "^(\/REMOTEEXEC\/)" + hostString + cgipathString;
 
-    });
-    extAllowed = (extAllowed.substring(0, extAllowed.length - 1)).concat(")$")
+    // Initializing regular expressions for each type of input
+    var localFileRegEx = new RegExp(localFileString);
+    var localExecRegEx = new RegExp(localExecString);
+    var remoteFileRegEx = new RegExp(remoteFileString);
+    var remoteExecRegEx = new RegExp(remoteExecString);
 
-    // example of creating new regex and testing it with a string
-    var regex1 = new RegExp(localFileString + filepathString + extAllowed);
-
-    var regex2 = new RegExp(remoteFileString + hostString + filepathString + extAllowed);
-
-    /*
-    var string2a = "/REMOTEFILE/myfile.html";
-    console.log(regex2.test(string2a)); //
-    */
-    var regex3 = new RegExp(localExecString + cgipathString);
-
-    var regex4 = new RegExp(remoteExecString + hostString + cgipathString);
-    //now we should know if the command was valid and if so what type it is
+    // Initilizing type to return value based on type of URL
     var type = 0;
-    if (regex1.test(input) == true){
-      type = 1;
 
+    // Test input with each regular expressions to identify type of URL
+    if (localFileRegEx.test(inputURL) == true) {
+        type = 1;
     }
-    else if(regex2.test(input) == true){
-      type = 2;
-
+    else if(localExecRegEx.test(inputURL) == true) {
+        type = 2;
     }
-    else if(regex3.test(input) == true){
-      type = 3;
+    else if(remoteFileRegEx.test(inputURL) == true) {
+        type = 3;
     }
-    else if(regex4.test(input) == true){
-      type = 4;
+    else if(remoteExecRegEx.test(inputURL) == true){
+        type = 4;
     }
-    //if it doesn't fall into any of these cases, i.e., it doesnt pass anything then it remains 0
-
+    // If it doesn't fall into any of these cases, remains 0
     return type;
 }
+
+
+function truncateURL(inputURL) {
+/* Function removes type of URL description from client URL by removing
+   characters until reaching the next '/'. */
+
+    var currChar = "";
+    var i = 1; // Start at first 'slash' in client URL
+    while (currChar != '/') {
+        currChar = inputURL[i];
+        i++;
+    }
+    var newUrl = xurl.substring(i, xurl.length); // Truncate url
+
+    return newURL;
+}
+
+
+function executePath(inputPath) {
+/* Function executes given path and writes corresponding output to server and
+   client in both success and error. */
+
+    // Execute file using callback function and sending output to client
+    exec(inputPath, function (err, stdout, stderr) {
+        if (err) {
+            response.statusCode = 403;
+            console.log("EXEC error: " + err);
+            response.write("Error executing file: " + stderr);
+        }
+        else {
+            response.write(stdout);
+        }
+        response.end();
+    });
+}
+
+
 function getURL(request, response) {
+/* Function that obtains client input and responds according to whether request
+   has been accepted or rejected. If accepted, accesses or executes requested
+   file. Rejects if file does not exist. */
+
     var xurl = request.url;
     response.statusCode = 200;
     // response.setHeader('Content-Type', 'text/plain');
     // response.write('You requested the following URL: '+xurl+'\n');
-    console.log("Hey, the client requested the URL: ("+xurl+")");
-
+    console.log("Hey, the client requested the URL: (" + xurl + ")");
     var type = process_url(xurl);
-    if (type != 0){
-      response.write("It has been ACCEPTED\n");
-      //response.end();
-    }else{
-      response.write("It has been REJECTED\n");
-      response.end();
-    }
-    if (type == 1){
-      //serve file
-      serveFile(xurl, response);
 
+    // Check if URL is valid and output corresponding response to server and client
+    if (type == 0) {
+        console.log("REJECTED");
+        response.write("Your URL is invalid and not accepted: " + xurl);
+        response.end();
     }
-    if (type == 2){
-      //serve remote file
-      pullAndsendFile(xurl, response);
-
+    else {
+        console.log("ACCEPTED");
+        if (type == 1) { // Accessing local file
+            serveFile(xurl, response);
+        }
+        else if (type == 2) { // Executing local CGI file
+            serveCGI(xurl, response);
+        }
+        else if (type == 3) { // Accessing remote file
+            pullandsendFile(xurl, response);
+        }
+        else if (type == 4) { // Executing remote CGI file
+            pullandsendOutput(xurl, response);
+        }
+        response.end();
     }
-    if (type == 3){
-      serveCGI(xurl, response);
-      //execfile
-    }
-    if (type == 4){
-      //exec remote file
-      pullandsendOutput(xurl, response);
-    }
-    //response.end();
-
 }
-function serveFile(xurl, response) {
-  /* your server shall create a file path from XelkReq.fileDir() + the URL requested.
-      This function must use the readFile() function for thispart.
-  */
-  var curr_char = ""
-  idx = 1; //start at first 'slash'
-  while (curr_char != '/'){
-    curr_char = xurl[idx];
-    idx ++;
-  }
-  var truncated_url = xurl.substring(idx - 1,xurl.length); //truncate url
 
-  //take the first character off until you hit the /
-  var filepath = XelkReq.fileDir().concat(truncated_url);
-  console.log(filepath);
-  var text;
-  var exp = ""
-  fs.readFile(filepath, function read(err, data) {
-    if (err) {
-        throw err;
-    }
-    response.write(data);
-    response.end();
+
+function serveFile(xurl, response) {
+/* Function creates a file path using XelkReq.fileDir() + requested URL and uses
+   readFile() to access the file. */
+
+    // Truncate URL
+    var truncatedUrl = "\/" + truncatedUrl(xurl);
+
+    // Creating file path
+    var filepath = XelkReq.fileDir().concat(truncatedUrl);
+    console.log(filepath);
+
+    // Read file using callback function and writing output to client
+    readFile(filepath, function read(err, data) {
+        if (err) {
+            response.statusCode = 403;
+            console.log("READ error: " + err);
+            response.write("Error reading file: " + err);
+        }
+        else {
+            response.write(data);
+        }
+        response.end();
     });
 }
 
+
 function serveCGI(xurl, response) {
-  /* your server shall use the exec() functionto run the requested .cgi file locally.
-    You shall usethe XelkReq.execdir() + the URL requested.
-  */
-  // response.setHeader('Content-Type', 'text/html');
-  var curr_char = ""
-  idx = 1;
-  while (curr_char != '/'){
-    curr_char = xurl[idx];
-    idx ++;
-  }
-  var truncated_url = xurl.substring(idx - 1,xurl.length);
+/* Function creates a file path using XelkReq.execDir() + requested URL and uses
+   exec() to execute the CGI file. */
 
-  //take the first character off until you hit the /
-  var execpath = XelkReq.execDir().concat(truncated_url);
-  console.log(execpath);
-  child_process.exec(execpath, function (error, stdout, stderr) {
-    if (error) {
-      //response.end("There was an error executing this CGI.");
-      console.log(stderr);
-    } else {
-      var text = stdout;
+    // Truncate URL
+    var truncatedURL = "\/" + truncateURL(xurl);
 
-      response.end(stdout)
-    }
-    // the response ending
-  });
+    // Create file path
+    var execpath = XelkReq.execDir().concat(truncatedUrl);
+    console.log(execpath);
+
+    // Execute path
+    executePath(execpath);
 }
 
-function pullAndsendFile(xurl, response) {
-  /* your server shall take therequested URL and use exec() to call "curl"+URL.
-  */
-  var curr_char = ""
-  idx = 1;
-  while (curr_char != '/'){
-    curr_char = xurl[idx];
-    idx ++;
-  }
-  var truncated_url = xurl.substring(idx, xurl.length);
 
-  //take the first character off until you hit the /
-  var execpath = truncated_url;
-  console.log(execpath);
-  child_process.exec("curl -s -S " + execpath, function (error, stdout, stderr) { //CHANGE CURL TO USR/BIN/CURL
-    if (error) {
-      response.end("There was an error executing this CGI.");
-      console.log(stderr);
-    } else {
-      var text = stdout;
+function pullandsendFile(xurl, response) {
+/* Function creates a curl command using requested URL and uses executePath() to
+   execute the curl command. */
 
-      response.end(stdout)
-    }
-    // the response ending
-  });
+    // Truncate URL
+    var truncatedURL = truncateURL(xurl);
 
+    // Create curl command using client URL
+    var execpath = "/usr/bin/curl -s -S " + truncatedUrl;
+    console.log(execpath);
 
+    // Execute path
+    executePath(execpath);
 }
 
-function pullandsendOutput(xurl, response){
-  /* your server shall take the requested URL and use exec() to call "curl"+URL forREMOTEEXEC.
-  Note, that pullandsendFile() andpullandsendOutput() are virtually identical.
-  Also note that serveCGI *also* uses exec().
-  You shouldnot duplicate code.
-  Hint:  you could write a function to exec() something passed in as a parameter.
-  That parameter could be either the CGI or either"curl" commands.....
-  */
-  var curr_char = ""
-  idx = 1;
-  while (curr_char != '/'){
-    curr_char = xurl[idx];
-    idx ++;
-  }
-  var truncated_url = xurl.substring(idx, xurl.length);
 
-  //take the first character off until you hit the /
-  var execpath = truncated_url;
-  console.log(execpath);
-  child_process.exec("curl -s -S " + execpath, function (error, stdout, stderr) { //CHANGE CURL TO USR/BIN/CURL
-    if (error) {
-      response.end("There was an error executing this CGI.");
-      console.log(stderr);
-    } else {
-      var text = stdout;
+function pullandsendOutput(xurl, response) {
+/* Function creates curl command using requested URL and uses executePath() to
+   execute the curl command. */
 
-      response.end(stdout)
-    }
-    // the response ending
-  });
+    // Truncate URL
+    var truncatedURL = truncateURL(xurl);
 
+    // Create curl command using client URL
+    var execpath = "usr/bin/curl -s -S " + truncatedURL;
+    console.log(execpath);
 
+    // Execute path
+    executePath(executePath);
 }
+
 
 function generatePort() {
-    var lower = XelkReq.LowerPort()
-    var upper = XelkReq.UpperPort()
+/* Function generates a random port number using XelkReq.LowerPort() and
+   XelkReq.UpperPort(), and then returns the port value. */
+    var lower = XelkReq.LowerPort();
+    var upper = XelkReq.UpperPort();
 
-    return Math.floor(Math.random() * (upper - lower)) + lower
+    return Math.floor(Math.random() * (upper - lower)) + lower;
 }
 
-var server = http.createServer(getURL).listen(8080);
 
-//const whereToListen = 'belgarath.cs.uky.edu';
-//const port = 3332;
-/*
-try{
-    server.on('error', (e) => {
-        console.log("Error! " +e.code);
-    }); // server.on()
-    //port = generatePort();
-    //whereToListen = "cs.uky.edu:";
-    server.listen(port,whereToListen);
-    console.log("Listening.....");
-} catch(error) {
-    console.log("There was an error!" +error);
-}
-
-*/
+// Creates a server to listen given a port and hostname, and uses a callback
+// function to output success or error.
+var server = http.createServer(getURL);
+server.listen(port, hostname, function(err) {
+    if (err) {
+        console.log(err);
+    }
+    else {
+        console.log("Server started. Listening on http://" + hostname + ":" + port);
+    }
+});
