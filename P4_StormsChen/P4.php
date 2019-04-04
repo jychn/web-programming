dac1c9b0b5c8bbe179b0c0f7cfa1711bce5a2813
Jacob Storms <jacob.storms@uky.edu>
2019-04-04 13:44:25 -0400

    Parsed JSON file and extracted list of searchables and labels. Extracted lists need to be checked still

--- P4_StormsChen/P4.php
+++ P4_StormsChen/P4.php
@@ -1,1 +1,36 @@
-# hello
+
+<?php
+error_reporting(E_ALL);
+
+$gamesstring = file_get_contents("http://www.cs.uky.edu/~paul/public/Games.json");
+
+$games = json_decode($gamesstring, true);
+//need label list
+//need searchables list
+$label_list = [];
+$searchables_list = [];
+foreach($games as $key => $value){
+  foreach($value as $element){
+    foreach($element as $ele_key => $ele_value){
+      //echo "$ele_key: ";
+      if ($ele_key == "label"){
+        array_push($label_list, $ele_key);
+      }
+      if (is_array($ele_value)){
+        foreach($ele_value as $elements){
+          array_push($searchables_list, $elements);
+          //echo "$elements ";
+        }
+        //echo "\n";
+      }
+      else{
+      //  echo "$ele_value\n";
+      }
+    }
+    //echo "\n";
+  }
+}
+array_unique($searchables_list);
+array_unique($label_list);
+
+?>
